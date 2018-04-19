3b4f84fcc37022631cf6fc7d5a4fdd4bdd038766
Sophie <aminsophie@gmail.com>
2018-04-19 12:46:29 -0600

    data persistence added, tableview draws for core data

--- Project3/ViewController.swift
+++ Project3/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import CoreData
 
 final class LocationAnnotation: NSObject, MKAnnotation {
     var coordinate: CLLocationCoordinate2D
@@ -47,8 +48,8 @@ class ViewController: UIViewController, UITableViewDataSource {
     var latlonString:String? = " "
     var latitude:Double = 0
     var longitude:Double = 0
-    private var data: [String] = []
-    private var latlon: [String] = [] //array of the combined latlon strings
+   // private var data: [String] = []
+    private var latlonArr: [String] = [] //array of the combined latlon strings
     
     
     override func viewDidLoad() {
@@ -68,19 +69,23 @@ class ViewController: UIViewController, UITableViewDataSource {
 //        }
         
         tableView.dataSource = self
-
+        loadDataToTableView()
     }
     
     //MARK: Actions
     @IBAction func enterLatLonPressed(_ sender: UIButton) {
         //process the lat and lon
         latlonString = getLatLon() //string
-        latitude = getLat()
-        longitude = getLon()
-        latlon.append(getLatLon())
-        tableView.reloadData()
+//        latitude = getLat()
+//        longitude = getLon()
+//        latlonArr.append(getLatLon())
+        setUpData(point: latlonString!)//prints the whole thing again?
+        latlonArr.append(latlonString!)
+      // loadDataToTableView()
+    
         //TODO: Find place on the map, load into core data
         showCoordinatesOnMap(lat: latitude, lon: longitude)
+        tableView.reloadData() //reload
         
     }
     
@@ -91,13 +96,13 @@ class ViewController: UIViewController, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return latlon.count
+        return latlonArr.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")! //1.
         
-        let text = latlon[indexPath.row] //2.
+        let text = latlonArr[indexPath.row] //2.
         
         cell.textLabel?.text = text //3.
         
@@ -137,6 +142,84 @@ class ViewController: UIViewController, UITableViewDataSource {
     
     mapView.addAnnotation(selectedAnnotation)
     mapView.setRegion(selectedAnnotation.region, animated: true)
+    }
+    
+    //CORE DATA
+    
+    func setUpData(point: String){
+        //        //1. Similar to create database and create SQL table named CarData (SQL comparisons)
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else{
+            return
+        } //instance of app delegate, check if it is the app delegate with a guard statement (safe code)
+        let mapPointContext = appDelegate.persistentContainer.viewContext
+        let mapPointEntity = NSEntityDescription.entity(forEntityName: "MapPoint", in: mapPointContext)
+        
+        //2. Similar to INSERT INTO CarData(color, price, type) VALUES(any, any, any) (SQL)
+        let newMapPoint = NSManagedObject(entity: mapPointEntity!, insertInto: mapPointContext)
+        //~database~ has been created
+        
+        newMapPoint.setValue(point, forKey: "latlon") //save one more object
+        
+        saveData(contextSaveObject: mapPointContext)
+        loadData(contextLoadObject: mapPointContext)
+    }
+    
+    func saveData(contextSaveObject: AnyObject){
+        do{
+            try contextSaveObject.save()
+        }
+        catch {
+            print("Error saving")
+        }
+    }
+    
+    
+    func loadData(contextLoadObject: AnyObject){
+        let myRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "MapPoint")
+        myRequest.returnsObjectsAsFaults = false
+        
+        //3. important part of Core Data to use the VALUES in the database
+        do{
+            let result = try contextLoadObject.fetch(myRequest)
+            for data in result as! [NSManagedObject]{ //cast as an array (for saving into rows)
+                
+                let theMapPoint = data.value(forKey: "latlon") as! String //must cast object to String (or Int if needed)
+              
+               
+                //the map point contains ALL the data values in core data
+                print("Here is my info from Database:\(String(describing: theMapPoint)) ")
+            }
+        }catch{
+            print("Error loading")
+        }
+        
+    }
+    
+    func loadDataToTableView(){
+        //        //1. Similar to create database and create SQL table named CarData (SQL comparisons)
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else{
+            return
+        } //instance of app delegate, check if it is the app delegate with a guard statement (safe code)
+        let mapPointContext = appDelegate.persistentContainer.viewContext
+        let mapPointEntity = NSEntityDescription.entity(forEntityName: "MapPoint", in: mapPointContext)
+        let myRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "MapPoint")
+        myRequest.returnsObjectsAsFaults = false
+        //fetch request for tableview data
+        //3. important part of Core Data to use the VALUES in the database
+        do{
+            let result = try mapPointContext.fetch(myRequest)
+            for data in result as! [NSManagedObject]{ //cast as an array (for saving into rows)
+               let theMapPoint = data.value(forKey: "latlon") as! String //must cast object to String (or Int if needed)
+                //the map point contains ALL the data values in core data
+                print("fetch values and put them in a string array")
+                latlonArr.append(theMapPoint)
+
+             //   print("Here is my info from Database:\(String(describing: theMapPoint!)) ")
+            }
+        }catch{
+            print("Error loading")
+        }
+
     }
     
 }
